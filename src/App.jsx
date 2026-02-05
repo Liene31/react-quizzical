a59a85a998f1db9fa0c40c22a003517edbf43ee5
Liene <lienebare@gmail.com>
2026-02-05 14:59:36 +0100

    add start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
+import { GamePlay } from "./GamePlay";
 
 //If btn is clicked, the boolean switch to true,
 //The start page closed and game page opens
 
 function App() {
+  // State variables
+  const [switchPage, setSwitchPage] = useState(false);
+
+  //Button - navigates to gamePlay and closes the start page
+  function startGame() {
+    setSwitchPage(true);
+  }
   return (
     <main>
-      <section className="start-page">
-        <h1>Quizzical</h1>
-        <p>Some description if needed</p>
-        <button>Start quiz</button>
-      </section>
-      <section className="quiz-page">
-        <h2>Question</h2>
-        <p>Question description</p>
-        <button>Check answers</button>
-      </section>
+      {!switchPage && (
+        <section className="start-page">
+          <h1>Quizzical</h1>
+          <p>Some description if needed</p>
+          <button onClick={startGame}>Start quiz</button>
+        </section>
+      )}
+      {switchPage && <GamePlay />}
     </main>
   );
 }
---
+++ src/GamePlay.jsx
@@ -0,0 +1,9 @@
+export const GamePlay = () => {
+  return (
+    <section className="game-play">
+      <h2>Question</h2>
+      <p>Question description</p>
+      <button>Check answers</button>
+    </section>
+  );
+};
