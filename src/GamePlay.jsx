71bc48a4174d651dd8e4646dbeba609d8ff836d4
Liene <lienebare@gmail.com>
2026-02-06 15:14:09 +0100

    add api with axios

--- src/GamePlay.jsx
+++ src/GamePlay.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const GamePlay = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`https://opentdb.com/api.php?amount=5&type=multiple`)
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <section className="game-play">
       <div className="question-container">
@@ -6,19 +22,7 @@ export const GamePlay = () => {
         <form>
           <div className="answers">
             <input type="radio" id="one" name="one" value="one" checked />
-            <label for="one">One</label>
-          </div>
-          <div className="answers">
-            <input type="radio" id="one" name="one" value="one" checked />
-            <label for="one">One</label>
-          </div>
-          <div className="answers">
-            <input type="radio" id="one" name="one" value="one" checked />
-            <label for="one">One</label>
-          </div>
-          <div className="answers">
-            <input type="radio" id="one" name="one" value="one" checked />
-            <label for="one">One</label>
+            <label htmlFor="one">One</label>
           </div>
         </form>
       </div>
