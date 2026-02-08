d15ea8c2b979db24b23fdef6e780114c5ded6b8f
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-02-08 14:04:43 +0100

    add question fetch from api

--- src/GamePlay.jsx
+++ src/GamePlay.jsx
@@ -1,31 +1,44 @@
+import { da } from "@faker-js/faker";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const GamePlay = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     axios
       .get(`https://opentdb.com/api.php?amount=5&type=multiple`)
       .then((res) => {
-        setData(res.data);
-        console.log(res.data);
+        setData(res.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+  if (data) {
+    console.log(data[0].incorrect_answers);
+  }
+
+  // RETURN
   return (
     <section className="game-play">
-      <div className="question-container">
-        <h2>How Many Hearts Does An Octopus Have?</h2>
-        <form>
-          <div className="answers">
-            <input type="radio" id="one" name="one" value="one" checked />
-            <label htmlFor="one">One</label>
+      {data?.map((item) => {
+        return (
+          <div className="question-container">
+            <h2>{item?.question}</h2>
+            <form>
+              {/*  */}
+              <div className="answers">
+                <input type="radio" id="one" name="one" value="one" checked />
+                <label htmlFor="one">{item?.incorrect_answers[0]}</label>
+              </div>
+              {/*  */}
+            </form>
           </div>
-        </form>
-      </div>
+        );
+      })}
+
       <button>Check answers</button>
     </section>
   );
